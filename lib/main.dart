1d2990a4d545e4577f63e381cf1a8a1d3be93c5b
UZAIR025 <133478189+UZAIR025@users.noreply.github.com>
2023-11-29 23:26:43 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,26 @@
-import 'package:daytask/Pages/home_page.dart';
-import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
+import 'dart:developer' as developer;
+import 'package:flutter/cupertino.dart';
+import 'package:bmi_calculator/pages/main_page.dart';
 
-void main() async {
-  await Hive.initFlutter("hive_boxes");
+void main() {
+  //developer.log("\x1B[37mIBMI App Starting\x1B[0m");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Day Task',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return SingleChildScrollView(
+      child: CupertinoApp(
+        title: 'IBMI',
+        routes: {
+          '/': (BuildContext _context) => MainPage(),
+        },
+        initialRoute: '/',
       ),
-      home: HomePage(),
     );
   }
 }
